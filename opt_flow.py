1a9b45daad84bc35cb4715b078537adb44b7c9ca
DESKTOP-KCB8N07\Yoongi Kim <naver7731@gmail.com>
2018-10-07 23:17:04 +0900

    클래스 기반으로 수정

--- opt_flow.py
+++ opt_flow.py
@@ -1,42 +1,49 @@
 import cv2
 import numpy as np
 
-RESIZE_WIDTH = 320
-RESIZE_HEIGHT = 180
+class OptFlow:
+    def __init__(self, resize_width=320, resize_height=180, height_start=0.2, height_end=0.5):
+        self.width = resize_width
+        self.height = resize_height
 
-half_height = int(RESIZE_HEIGHT / 2)
+        self.height_start = int(self.height * height_start)
+        self.height_end = int(self.height * height_end)
 
-cap = cv2.VideoCapture("data/test.mp4")
-ret, img1 = cap.read()
-frame1 = cv2.resize(img1, (RESIZE_WIDTH, RESIZE_HEIGHT))
-prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    def get_direction(self, frame1, frame2):
+        frame1 = cv2.resize(frame1, (self.width, self.height))
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        frame2 = cv2.resize(frame2, (self.width, self.height))
+        frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-while(1):
+        flow = cv2.calcOpticalFlowFarneback(frame1[self.height_start:self.height_end],
+                                            frame2[self.height_start:self.height_end], None, 0.5, 3, 15, 1, 5, 1.2, 0)
+        flow_avg = np.median(flow, axis=(0, 1))  # [x, y]
+
+        move_x = -1 * flow_avg[0]
+        move_y = -1 * flow_avg[1]
+
+        return move_x, move_y
+
+if __name__ == '__main__':
+    flow = OptFlow()
+
+    cap = cv2.VideoCapture("C:/Users/Yoongi Kim/Videos/Captures/Grand Theft Auto V 2018-10-07 오후 10_31_38.mp4")
+    _, img1 = cap.read()
     _, img2 = cap.read()
-    frame2 = cv2.resize(img2, (RESIZE_WIDTH, RESIZE_HEIGHT))
-    next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-    flow = cv2.calcOpticalFlowFarneback(prvs[half_height:],next[half_height:], None, 0.5, 3, 15, 1, 5, 1.2, 0)
-    flow_avg = np.median(flow, axis=(0,1)) # [x, y]
-    print('X:%d, Y:%d' % (-1*flow_avg[0], flow_avg[1]))
-    move_x = -1*flow_avg[0]
-    move_y = -1*flow_avg[1]
 
-    h, w, c = img2.shape
-    arrow = cv2.arrowedLine(img2, (int(w/2), int(h/2)), (int(w/2+move_x*15), int(h/2+move_y*15)), color=(0,255,255), thickness=15)
-    cv2.imshow('arrow', arrow)
+    while True:
+        x, y = flow.get_direction(img1, img2)
+        print('X: {}, Y: {}'.format(x, y))
 
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv = np.zeros((half_height, RESIZE_WIDTH, 3), dtype=np.uint8)
-    hsv[..., 1] = 255
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-    cv2.imshow('map',bgr)
-    cv2.moveWindow('arrow', 0, 0)
-    cv2.moveWindow('map', 0, 420)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print('User Interrupted')
-        break
-    prvs = next
-cap.release()
-cv2.destroyAllWindows()
+        h, w, c = img2.shape
+        arrow = cv2.arrowedLine(img2, (int(w / 2), int(h / 2)), (int(w / 2 + x * 25), int(h / 2 + -1*abs(y * 50))),
+                                color=(0, 255, 255), thickness=15)
+
+        cv2.imshow('arrow', arrow)
+        cv2.moveWindow('arrow', 0, 0)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print('User Interrupted')
+            break
+
+        img1 = img2
+        _, img2 = cap.read()
